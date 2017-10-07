27ac60b0239fa82239740b1d63735f2af742a7da
Jonathan <jonathankim1228@hotmail.com>
2017-10-06 18:21:47 -0700

    create basic form and make sure controller receives the data

--- app/scripts/controllers/landingPage.js
+++ app/scripts/controllers/landingPage.js
@@ -1,3 +1,6 @@
 angular.module('leadsApp').controller('landingPageCtrl', function ($scope) {
   $scope.landingPage = "LANDINGPAGE"
+  $scope.submit = function(){
+    console.log($scope.leadsForm);
+  }
 });
