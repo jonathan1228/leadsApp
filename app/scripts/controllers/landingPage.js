1cc9fcb5d2b61ddc57aa815565bcc36e5945b786
Jonathan <jonathankim1228@hotmail.com>
2017-10-06 23:14:35 -0700

    minor fixes of fixing submit button bug and ordering list in alphabetical order

--- app/scripts/controllers/landingPage.js
+++ app/scripts/controllers/landingPage.js
@@ -11,7 +11,6 @@ angular.module('leadsApp').controller('landingPageCtrl', function ($scope, $http
 
   $scope.getInitialID = function(){
     $http.post('/landingPage/getInitId', $scope.leadsForm).then(function(result){
-      console.log("testing");
       console.log(result);
       $scope.leadsForm.id = result.data.id;
     }).catch(function(err){
@@ -29,6 +28,7 @@ angular.module('leadsApp').controller('landingPageCtrl', function ($scope, $http
 
   $scope.submit = function(){
     $http.post('/landingPage/add', $scope.leadsForm).then(function(){
+      console.log("testing");
       $window.location.reload();
     }).catch(function(err){
       console.log("ERROR", err)
--- tests/landingPage.controller.test.js
+++ tests/landingPage.controller.test.js
@@ -34,31 +34,52 @@ describe('MyController', function(){
 	// 	}));
 	// });
 
-// describe('getInitialID()', function(){
-//         it('should return with an id', inject(function($rootScope, $controller, $httpBackend, $http){
-//       var mockedData =  {
-//                       firstName: '',
-//                       lastName: '',
-//                       phoneNumber: '',
-//                       email: '',
-//                       address: '',
-//                       homeSquareFootage: null
-//             }
-//       scope = $rootScope.$new();
-//       httpBackend = $httpBackend;
-//       http = $http;
-//       controller = $controller;
-//       httpBackend.when('POST', '/landingPage/getInitId', mockedData).respond(1);
-//
-//
-//             $controller('landingPageCtrl',{
-//             $scope: scope,
-//             $http: http
-//           });
-//       console.log(scope.leadsForm);
-//       scope.getInitialID()
-//       httpBackend.flush()
-//             expect(scope.leadsForm.id).to.equal(3);
-//
-//         }));
-//     });
+describe('getInitialID()', function(){
+  var $httpBackend, $rootScope, createController
+  beforeEach(inject(function($injector) {
+     // Set up the mock http service responses
+     $httpBackend = $injector.get('$httpBackend');
+     // backend definition common for all tests
+
+     // Get hold of a scope (i.e. the root scope)
+     $rootScope = $injector.get('$rootScope');
+     // The $controller service is used to create instances of controllers
+     var $controller = $injector.get('$controller');
+
+     createController = function() {
+       return $controller('landingPageCtrl', {'$scope' : $rootScope });
+     };
+   }));
+
+
+   afterEach(function() {
+     $httpBackend.verifyNoOutstandingExpectation();
+     $httpBackend.verifyNoOutstandingRequest();
+   });
+
+        it('should return with an id', inject(function($rootScope, $controller, $httpBackend, $http){
+      var mockedData =  {
+                      firstName: '',
+                      lastName: '',
+                      phoneNumber: '',
+                      email: '',
+                      address: '',
+                      homeSquareFootage: null
+            }
+
+            var controller = createController();
+
+             // now you don’t care about the authentication, but
+             // the controller will still send the request and
+             // $httpBackend will respond without you having to
+             // specify the expectation and response for this request
+
+             $httpBackend.expectPOST('/landingPage/getInitId', mockedData).respond(200, 1);
+             $rootScope.getInitialID('test');
+             console.log($rootScope.leadsForm.id);
+             expect($rootScope.leadsForm.id).toBe('Saving...');
+             $httpBackend.flush();
+
+        }));
+    });
+})
